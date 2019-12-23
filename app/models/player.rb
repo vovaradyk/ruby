2306c76903ee438245f86f658c7bbe3478cf4a70
Andrew <blekoniblekoni@gmail.com>
2019-12-23 14:47:07 +0200

    first commit:

--- app/models/player.rb
+++
@@ -1,7 +0,0 @@
-class Player < ApplicationRecord
-  belongs_to :team
-  validates :name, :presence => {:message => "Вкажіть ім'я"}
-  validates :height, numericality: {greater_than: 0, :message => "3pіст повинен бути більшим за 0"}
-  validates :weight, numericality: {greater_than: 0, :message => "Вага повинна бути більшою за 0"}
-  validates :birthday, :presence => {:message => "Вкажіть дату народження"}
-end
